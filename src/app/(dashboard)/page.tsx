36d78b8c98b10cfb7de9703725e6e0567f4ede2a
Jama1B <jamyjay4@gmail.com>
2025-06-05 20:34:19 +0300

    make src main

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,17 @@
+import { OverviewCard } from "@/components/OverviewCard";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+
+export default async function Home() {
+  const user = await currentUser();
+  const { userId } = await auth();
+
+  return (
+    <div>
+      <div className="flex flex-col space-y-4 p-5 ">
+        <h1 className="font-semibold text-5xl">Welcome, {user?.firstName}!</h1>
+        <OverviewCard userid={userId} />
+      </div>
+    </div>
+  );
+}
